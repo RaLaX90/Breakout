4bc58a62e70bb9bbd6592d3bcb2af80b7d088eaa
DESKTOP-CL8GBLN\Олександр <cool.suhomlin1@gmail.com>
2022-07-29 20:48:42 +0300

    First commit.

---
+++ Breakout/Breakout.cpp
@@ -0,0 +1,12 @@
+﻿// Breakout.cpp : Defines the entry point for the application.
+//
+
+#include "Breakout.h"
+
+using namespace std;
+
+int main()
+{
+	cout << "Hello CMake." << endl;
+	return 0;
+}
